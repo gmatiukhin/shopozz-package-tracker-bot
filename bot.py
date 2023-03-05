37a6ae39d09bdaa7a443d35ea6a3230a3d219f7d
Grigorii Matiukhin <grigory36m@gmail.com>
2023-03-05 22:00:18 +0300

    Telegram bot, minor adjustments in scraping code

---
+++ bot.py
@@ -0,0 +1,90 @@
+import logging
+from datetime import timedelta
+
+from telegram import Update
+from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler
+
+from tracking import Tracklist
+tracklist = Tracklist()
+
+import scraper
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    assert update.effective_chat is not None
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide a tracking number using /track command")
+
+async def new_tracking_number(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if context.args and update.effective_chat is not None:
+        chat_id = update.effective_chat.id
+        tracking_number = context.args[0]
+        tracklist.add(chat_id, tracking_number)
+
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Done, I am traking your package.")
+
+async def invalid_tracking_number(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    assert update.effective_chat is not None
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, your track number is invalid.")
+
+async def remove_tracking_number(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if context.args and update.effective_chat is not None:
+        chat_id = update.effective_chat.id
+        tracking_number = context.args[0]
+        tracklist.remove(chat_id, tracking_number)
+
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Done, I am no longer tracking your package.")
+
+async def unknown_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    assert update.effective_chat is not None
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I did not understand that command.")
+
+async def unknown_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    assert update.effective_chat is not None
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I do not know how to respond.")
+
+async def tracking_status_check(context: ContextTypes.DEFAULT_TYPE):
+    tracking_data = tracklist.data()
+    for chat_id, tracking_numbers in tracking_data.items():
+        for number in tracking_numbers:
+            message = scraper.get_status(number)
+            if message:
+                await context.bot.send_message(chat_id=chat_id, text=message)
+
+
+if __name__ == '__main__':
+    tracklist.deserialize()
+
+    with open("bot-token", "r") as token_file:
+        token = token_file.read().strip()
+
+    application = ApplicationBuilder().token(f"{token}").build()
+
+    job_queue = application.job_queue
+    assert job_queue is not None
+    job_minute = job_queue.run_repeating(tracking_status_check, interval=timedelta(minutes=1))
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    new_tracking_number_handler = CommandHandler("track", new_tracking_number, filters.Regex(r"[A-Z]{2}\d{9}RU"))
+    application.add_handler(new_tracking_number_handler)
+
+    invalid_tracking_number_handler = CommandHandler("track", invalid_tracking_number)
+    application.add_handler(invalid_tracking_number_handler)
+
+    remove_tracking_number_handler = CommandHandler("untrack", remove_tracking_number, filters.Regex(r"[A-Z]{2}\d{9}RU"))
+    application.add_handler(remove_tracking_number_handler)
+
+    # Unknown handlers
+    unknown_command_handler = MessageHandler(filters.COMMAND, unknown_command)
+    application.add_handler(unknown_command_handler)
+
+    unknown_message_handler = MessageHandler(filters.ALL, unknown_message)
+    application.add_handler(unknown_message_handler)
+
+
+    application.run_polling()
--- scraper.py
+++ scraper.py
@@ -1,12 +1,6 @@
 import requests
 import json
-import sys
 from datetime import datetime
-
-def main():
-    tracking_number = sys.argv[1]
-    status_message = get_status(tracking_number)
-    print(status_message)
 
 def get_status(tracking_number):
     data = get_russian_post_data(tracking_number)
@@ -17,10 +11,11 @@ def get_status(tracking_number):
         data = get_shopozz_data(tracking_number)
         if data:
             return format_shopozz_event(data[0])
+    return ""
 
-def format_event(date, event, location):
+def format_event(date, event, location, source):
     date = date.strftime("%Y-%m-%d %H-%M-%S")
-    message = f"{event}\n{date}\n{location}"
+    message = f"{event}\n{date}\n{location}\n~ {source}"
     return message
 
 def format_russian_post_event(json_data):
@@ -29,16 +24,16 @@ def format_russian_post_event(json_data):
     city = json_data["cityName"]
     country = json_data["countryName"]
     location = ""
-    if not city:
+    if city:
         location += city + ", "
     location += country
-    return format_event(date, event, location)
+    return format_event(date, event, location, "pochta.ru")
 
 def format_shopozz_event(json_data):
     date = datetime.fromisoformat(json_data["datetime"])
     event = json_data["event"]
     location = json_data["location"]
-    return format_event(date, event, location)
+    return format_event(date, event, location, "Shopozz.ru")
 
 def get_shopozz_data(tracking_number):
     url = f"https://shopozz.ru/tracking/{tracking_number}?ajax=true"
@@ -47,15 +42,11 @@ def get_shopozz_data(tracking_number):
     }
     response = requests.get(url, headers=headers)
 
-    with open("shopozz.json", "w") as file:
-        file.write(response.text)
-
     data = json.loads(response.text)
     try:
         return data["events"]
     except KeyError:
         return []
-
 
 def get_russian_post_data(tracking_number):
     url = "https://www.pochta.ru/api/nano-apps/api/v1/tracking.get-by-barcodes?language=ru"
@@ -68,12 +59,5 @@ def get_russian_post_data(tracking_number):
 
     response = requests.post(url, json=payload, headers=headers, allow_redirects=True)
 
-    with open("ru-post.json", "w") as file:
-        file.write(response.text)
-
     data = json.loads(response.text)
     return data["detailedTrackings"][0]["trackingItem"]["trackingHistoryItemList"]
-
-
-if __name__ == "__main__":
-    main()
