2fc9e143a81d2306196478412989fe118bc9dd80
Grigorii Matiukhin <grigory36m@gmail.com>
2023-03-22 15:20:18 +0300

    fix crash on bad response from post

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ import requests
 import json
 from datetime import datetime
 
+
 def get_status(tracking_number):
     data = get_russian_post_data(tracking_number)
 
@@ -13,10 +14,12 @@ def get_status(tracking_number):
             return format_shopozz_event(data[0])
     return ""
 
+
 def format_event(date, event, location, source):
     date = date.strftime("%Y-%m-%d %H-%M-%S")
     message = f"{event}\n{date}\n{location}\n~ {source}"
     return message
+
 
 def format_russian_post_event(json_data):
     date = datetime.fromisoformat(json_data["date"])
@@ -29,11 +32,13 @@ def format_russian_post_event(json_data):
     location += country
     return format_event(date, event, location, "pochta.ru")
 
+
 def format_shopozz_event(json_data):
     date = datetime.fromisoformat(json_data["datetime"])
     event = json_data["event"]
     location = json_data["location"]
     return format_event(date, event, location, "Shopozz.ru")
+
 
 def get_shopozz_data(tracking_number):
     url = f"https://shopozz.ru/tracking/{tracking_number}?ajax=true"
@@ -48,6 +53,7 @@ def get_shopozz_data(tracking_number):
     except KeyError:
         return []
 
+
 def get_russian_post_data(tracking_number):
     url = "https://www.pochta.ru/api/nano-apps/api/v1/tracking.get-by-barcodes?language=ru"
     payload = [str(tracking_number)]
@@ -58,6 +64,7 @@ def get_russian_post_data(tracking_number):
     }
 
     response = requests.post(url, json=payload, headers=headers, allow_redirects=True)
-
-    data = json.loads(response.text)
-    return data["detailedTrackings"][0]["trackingItem"]["trackingHistoryItemList"]
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        return data["detailedTrackings"][0]["trackingItem"]["trackingHistoryItemList"]
+    return None
